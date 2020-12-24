06fcaef70865af6bc0c567cefb21349541c5bcbf
Svante Pihl <hej@svantepihl.com>
2020-12-24 17:39:05 +0100

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Tailwind CSS ➕ Emotion Starter`,
+    description: `A bare-bones Tailwind CSS + Emotion starter to kickoff your project.`,
+    author: `@pauloelias`,
+  },
+  plugins: [
+    `gatsby-plugin-emotion`,
+    `gatsby-plugin-postcss`,
+    `gatsby-plugin-react-helmet`,
+  ],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,50 @@
+
+import tw from "twin.macro"
+import React from "react"
+
+import "@fontsource/mountains-of-christmas"
+import SEO from "../components/seo"
+import Layout from "../components/layout"
+
+const Wrapper = tw.div`
+  flex items-center justify-center flex-col h-screen
+`
+
+const Main = tw.div`
+  p-6 bg-gray-100 rounded-lg shadow-2xl lg:w-1/2 w-full
+`
+
+const Heading = tw.h1`
+  text-2xl text-black
+`
+
+const Text = tw.p`
+  text-xl text-black
+`
+
+
+export default function Index() {
+  return (
+    <Layout>
+      <Wrapper>
+        <SEO title="God jul!" />
+        <Main>
+          <Heading>GOD JUL KRISTINA <br/>Önskar Tomten<br/></Heading>
+          <Text className="pt-12 text-left">
+          <br/>Nu tändas tusen juleljus<br/>
+          på ikeahylla i klostergårdshus <br/><br/>
+
+          Du från Dalby på el-cykel rullar <br/>
+          över kullar och genom stadstullar <br/><br/>
+
+          Synas i mörkret det är bäst <br/>
+          men din gula väst <br/>
+          vill man ej ha på fest <br/>
+          denna är mer modest men syns mest
+          </Text>
+
+        </Main>
+      </Wrapper>
+    </Layout>
+  )
+}
